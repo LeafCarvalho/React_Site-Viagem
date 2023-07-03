8d2d764029708390f4c2dc9b99f4e422bd51a388
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2023-07-03 18:08:08 -0300

    alterando configurações do projeto para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
+  base: '/React_Site-Viagem/'
 })
