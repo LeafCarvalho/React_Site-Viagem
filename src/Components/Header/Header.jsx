1c0ac6c61525538daf9cb3b1e3ee8ade8bc1d241
Rafael Carvalho <rafaeldecarvalhodasilva01@yahoo.com.br>
2023-07-28 14:27:33 -0300

    Deixando código mais limpo com Link ao invés de useNavigate

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,27 +5,27 @@ import logo from '../../Assets/Logo/logo.png'
 import styles from './Header.module.css'
 
 // HOOKS
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
-  const navigate = useNavigate();
+
   return (
     <Navbar collapseOnSelect expand="lg" className={styles.header}>
-      <Container>
+      <Container fluid className={styles.content}>
         <Navbar.Brand onClick={() => {navigate('/')}} className={styles.logo}>
           <img src={logo} alt="" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-          </Nav>
-          <Nav>
-            <Nav.Link eventKey={2} onClick={() => {navigate('/')}}>
-            Home
-            </Nav.Link>
-            <Nav.Link eventKey={3} onClick={() => {navigate('/contact')}}>
-            Contato
-            </Nav.Link>
+          <Nav className="ms-auto">
+            <Link to={"/"} className="nav-link"> {/* Adicionando a classe "nav-link" */}
+              Home
+            </Link>
+            {/* Adicionando o espaçamento entre as opções */}
+            <span style={{ marginRight: '1rem' }}></span>
+            <Link to={"/contact"} className="nav-link"> {/* Adicionando a classe "nav-link" */}
+              Contato
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
